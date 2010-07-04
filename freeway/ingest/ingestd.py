ee9145d1163e8071bc07682af04fae4ec0e6d699
drewp <drewp@bigasterisk.com>
2010-07-04 16:07:28 -0700

    ingest sensor position data and sensor readings to mongo
    Ignore-this: 2b66bad8ae8223fb142f5ff504c2fb76
    
    darcs-hash:20100704230728-312f9-35457780e31e6f1d988cfefe911f973f754d5fde.gz

---
+++ freeway/ingest/ingestd.py
@@ -0,0 +1,84 @@
+import os, time, datetime, dateutil.tz
+import cPickle as pickle
+from glob import glob
+from twisted.internet import reactor
+from pymongo import bson, Connection
+
+from loop_data import parse5MinFile
+from detector import parseVdsConfig
+from memoize import lru_cache
+
+class UpdateLoop(object):
+    """
+    task that fetches measurements into mongo every 5 minutes
+    """
+    def __init__(self):
+        self.db = Connection('bang', 27017)['freeway']
+        self.update()
+
+    def update(self):
+        now = time.time()
+        try:
+            print "fetching:"
+            ret = os.system("./ftp_get")
+            print "done", ret
+
+            timestamp, samples = parse5MinFile("spool/5minagg_latest.txt.gz")
+            print "now %s, datatime %s" % (now, timestamp)
+            nextDataTime = timestamp + 5 * 60
+            mongoSave(self.db, timestamp, now, samples)
+        except Exception, e:
+            print "update failed:", e
+            nextDataTime = now
+
+        # it would be nice to get in phase with their data, but their
+        # timestamp is much older than my current clock (like 9.5
+        # mins, today) so I'm not sure what to do
+        delay = 5*60# max(5, nextDataTime - now)
+        
+        print "next download in %s sec" % delay
+        reactor.callLater(delay, self.update)
+
+def mongoFromTimestamp(t):
+    return datetime.datetime.fromtimestamp(t, dateutil.tz.tzutc())
+
+def mongoSave(db, t, fetchTime, pts):
+    for pt in pts:
+        pt['time'] = mongoFromTimestamp(t)
+        pt['fetchTime'] = mongoFromTimestamp(fetchTime)
+        pt['num_samples'] = int(pt['num_samples'])
+        del pt['travel_time'] # docs: "not in use"
+
+        for k in ['delay', 'pct_observed', 'vht', 'vmt', 'flow',
+                  'occupancy', 'q', 'speed',]:
+            try:
+                pt[k] = float(pt[k])
+            except ValueError: # empty string?
+                pt[k] = None
+        db['meas'].save(pt)
+
+
+def getRecentSets(n=5):
+    """most recent time,measurements first, then n-1 more of those pairs"""
+    for filename in recentFilenames(n):
+        timestamp, meas = loadFile(filename)
+        yield timestamp, meas
+
+@lru_cache(40)
+def loadFile(filename):
+    f = open(filename)
+    timestamp, meas = pickle.load(f)
+    f.close()
+    return timestamp, meas
+
+def filenameForTime(t):
+    s = str(int(t))
+    w1, w2 = s[:-5], s[-5:]
+    return "spool/meas.%s/%s" % (w1, s)
+
+def recentFilenames(n):
+    times = []
+    for recentDir in sorted(glob("spool/meas.*"))[-2:]:
+        times.extend(glob("%s/*" % recentDir))
+    times.sort(reverse=True)
+    return times[:n]
